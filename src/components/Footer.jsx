5cef7ba1344ebf8589e6fdf2d790f447f56ffe1e
Tanatorn-hub <jaysiam46@gmail.com>
2025-03-17 16:14:21 +0700

    ฝึกทำ component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <>
+        <br /><br /><hr /><br />
+        <div style={{textAlign:"center"}}>
+            <h5>Copyright &copy; DTI-SAU 2025</h5>
+            <h5>All Right Reserved, Create by Jay 😊</h5>
+        </div>
+    </>
+  )
+}
+
+export default Footer
